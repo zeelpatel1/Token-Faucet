b13a2cd1fb021061133bc3696f0b03b8d6cc539b
zeelpatel1 <zeelpatel6878@gmail.com>
2025-11-29 12:41:54 +0000

    initialize-faucet

---
+++ programs/token-faucet/src/lib.rs
@@ -0,0 +1,68 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self,Mint,TokenAccount,Transfer,Token};
+
+declare_id!("DyTqqVKtYEzJAdJhT2aDcVuTD7JFkRWjBDfeXTCZWPUe");
+
+#[program]
+pub mod token_faucet {
+    use super::*;
+
+    pub fn initialize_faucet(ctx:Context<InitializeFaucet>,mint:Pubkey,drip_amount:u64,cooldown_seconds:u64) -> Result<()> {
+
+        let faucet=&mut ctx.accounts.faucet;
+        faucet.admin=ctx.accounts.user.key();
+        faucet.mint=ctx.accounts.user_token_mint.key();
+        faucet.drip_amount=drip_amount;
+        faucet.cooldown_seconds=cooldown_seconds;
+
+        faucet.bump = ctx.bumps.faucet;
+
+        Ok(())
+    }
+
+}
+
+#[derive(Accounts)]
+pub struct InitializeFaucet<'info>{
+
+    #[account(mut)]
+    pub user:Signer<'info>,
+
+    pub user_token_mint:Account<'info,Mint>,
+
+    #[account(mut)]
+    pub user_token_ata:Account<'info,TokenAccount>,
+
+    #[account(
+        init,
+        payer=user,
+        space= 8 + 32 + 32 + 8 + 8 + 1,
+        seeds=[b"faucet_metadata",user_token_mint.key().as_ref()],
+        bump
+    )]
+    pub faucet:Account<'info,Faucet>,
+
+    #[account(
+        init,
+        payer=user,
+        seeds=[b"faucet_vault",user_token_mint.key().as_ref()],
+        bump,
+        token::mint=user_token_mint,
+        token::authority=faucet
+    )]
+    pub vault_faucet:Account<'info,TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+
+}
+
+#[account]
+pub struct Faucet {
+    pub admin:Pubkey,
+    pub mint:Pubkey,
+    pub drip_amount:u64,
+    pub cooldown_seconds:u64,
+    pub bump:u8
+}
